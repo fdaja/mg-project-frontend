e4d1d9e23ea3465722171f6bd3fc205bce9d87f1
Franko <dajafranko@gmail.com>
2025-01-11 17:35:42 +0100

    Add production build folder for deployment

---
+++ build/precache-manifest.f33a489f33ebed2da1f3dfce20c6e757.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9066c3d93307cff6890b5964ebee11f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d282fcb2e7eefdf0271",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5ae08b0b49acb56e1603",
+    "url": "/static/js/2.bf12104a.chunk.js"
+  },
+  {
+    "revision": "c01bfd9d4ec356c3c960d8f7db9c80a4",
+    "url": "/static/js/2.bf12104a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8532c38c8b1ef8a22e0",
+    "url": "/static/js/3.2e74881a.chunk.js"
+  },
+  {
+    "revision": "5d282fcb2e7eefdf0271",
+    "url": "/static/js/main.e39160da.chunk.js"
+  },
+  {
+    "revision": "92616ed663b4c15c52ee",
+    "url": "/static/js/runtime-main.f8c280a3.js"
+  }
+]);
